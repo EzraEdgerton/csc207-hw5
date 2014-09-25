80588a1cdb32dc01d8402ced2eb75977107d05a0
charlie-a-gao <gaocharl17@grinnell.edu>
2014-09-24 23:17:31 -0500

    added 2 exception classes

---
+++ src/edu/grinnell/csc207/groupgroup/utils/InvalidInputStringException.java
@@ -0,0 +1,12 @@
+package edu.grinnell.csc207.groupgroup.utils;
+
+public class InvalidInputStringException extends Exception {
+
+	public InvalidInputStringException() {
+		
+	}
+
+	public InvalidInputStringException(String message) {
+		super(message);
+	}
+}
